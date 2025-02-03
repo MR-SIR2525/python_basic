100be38df2c9da7f345057d7013068b6a422dc80
MR-SIR2525 <95254450+MR-SIR2525@users.noreply.github.com>
2025-02-03 16:20:01 -0600

    commit workspace file

---
+++ workspace.py
@@ -0,0 +1,12 @@
+
+# The loop doesn't print 16 because the range() function generates numbers up to
+# but not including the stop value (16 in this case).
+
+for i in range(12, 16):
+    print(i)
+
+
+# The syntax to create a virtual environment in PowerShell is:
+# python -m pip install --user virtualenv
+# python -m virtualenv env
+# .\env\Scripts\activate
